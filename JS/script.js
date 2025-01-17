d5c49d7349a57bd190c6e11e8cd5a23933d32658
nicolo_canevari <nico.canevari@outlook.it>
2025-01-17 15:25:07 +0100

    [FEAT] JS: items + [FIX] bug "img" in js che non lo leggeva

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,40 @@
+// array team
 const teamMembers = [
   {
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    image: "img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    image: "img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    image: "img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    image: "img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    image: "img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    image: "img/female3.png"
   }
 ];
 
@@ -47,9 +48,35 @@ let items = "";
 for (let i = 0; i < teamMembers.length; i++) {
   let teamIesimo = teamMembers[i];
 
-  const { name, role, email, img } = teamIesimo;
+  const { name, role, email, image } = teamIesimo;
 
   // console.log(name, role, email, img);
 
+
+  items + - `
+
+    < div id = "team-container" >
+
+        <div id="img-container">
+
+            <img src="img/${image}" alt="${name}" />
+
+        </div>
+
+        <div id="text-container">
+
+            <h3>${name}</h3>
+
+            <span>${role}</span>
+
+            <span>${email}</span>
+
+        </div>
+
+    </div >
+
+    `
+
+  console.table(items);
 
 }
